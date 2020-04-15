30d9a43de7b49a2a0ec38dd1970303afb81b0db7
Alejandro Cerrato Espejo <alesagustin@gmail.com>
2020-04-15 20:59:50 +0200

    Created the JSONNode class to work with json nodes, extends from GenericNode.

--- src/main/java/me/cerratolabs/io/file/configloader/configuration/adapters/json/JSONNode.java
+++ src/main/java/me/cerratolabs/io/file/configloader/configuration/adapters/json/JSONNode.java
@@ -1,4 +1,19 @@
 package me.cerratolabs.io.file.configloader.configuration.adapters.json;
 
-public class JSONNode {
+import me.cerratolabs.io.file.configloader.configuration.adapters.generic.GenericNode;
+
+import java.util.HashMap;
+
+/**
+ * Class used to handle the JSON file map.
+ * This class extends from 'YAMLNode' since the operation is exactly the same as in the other class.
+ *
+ * @author Alejandro '@Katakurinna' Cerrato Espejo
+ * @version 1.0.0
+ */
+public class JSONNode extends GenericNode {
+
+    public JSONNode() {
+        map = new HashMap<>();
+    }
 }
